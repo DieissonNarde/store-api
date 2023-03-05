dc0b974c05f47c68ad37165e5ad8027bcde8d3c9
DieissonNarde <dieissonnarde@gmail.com>
2023-03-04 20:50:36 -0400

    Aula 18: supplier and product repository

--- services/product.service.js
+++ services/product.service.js
@@ -1,8 +1,9 @@
 import ProductRepository from '../repositories/product.repository.js';
 import SupplierRepository from '../repositories/supplier.repository.js';
+import SaleRepository from '../repositories/sale.repository.js';
 
 async function createProduct(product) {
-  if (await SupplierRepository.getSupplier(product.supplier_id)) {
+  if (await SupplierRepository.getSupplier(product.supplierId)) {
     return await ProductRepository.insertProduct(product);
   }
 
@@ -18,11 +19,15 @@ async function getProduct(id) {
 }
 
 async function deleteProduct(id) {
+  // const sales = await SaleRepository.getSalesByProductId(id);
+  // if (sales) {
+  //   throw new Error('Não é possível excluir o produto pois ele possui vendas.');
+  // }
   await ProductRepository.deleteProduct(id);
 }
 
 async function updateProduct(product) {
-  if (await SupplierRepository.getSupplier(product.supplier_id)) {
+  if (await SupplierRepository.getSupplier(product.supplierId)) {
     return await ProductRepository.updateProduct(product);
   }
 
